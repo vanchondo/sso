176148c792bb6c588d9c93ea442c69f1da19232e
Victor Anchondo <eduardoanchondo@gmail.com>
2023-06-21 08:09:57 -0600

    Adding email validation template

--- src/main/java/com/vanchondo/sso/services/EmailService.java
+++ src/main/java/com/vanchondo/sso/services/EmailService.java
@@ -1,10 +1,29 @@
 package com.vanchondo.sso.services;
 
 import com.vanchondo.sso.configs.properties.EmailConfiguration;
-import jakarta.mail.*;
-import jakarta.mail.internet.*;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import freemarker.template.TemplateExceptionHandler;
+import jakarta.mail.Authenticator;
+import jakarta.mail.Message;
+import jakarta.mail.MessagingException;
+import jakarta.mail.Multipart;
+import jakarta.mail.PasswordAuthentication;
+import jakarta.mail.Session;
+import jakarta.mail.Transport;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeBodyPart;
+import jakarta.mail.internet.MimeMessage;
+import jakarta.mail.internet.MimeMultipart;
+import java.util.Map;
 import org.springframework.stereotype.Service;
 import lombok.extern.log4j.Log4j2;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Locale;
 
 @Service
 @Log4j2
@@ -25,15 +44,15 @@ public class EmailService {
         });
     }
 
-    public void sendEmail(String toEmail) throws MessagingException {
+    public void sendEmail(String toEmail) throws MessagingException, TemplateException, IOException {
         Message message = new MimeMessage(session);
         message.setFrom(new InternetAddress(emailConfiguration.getUsername()));
         message.setRecipients(
                 Message.RecipientType.TO, InternetAddress.parse(toEmail));
-        message.setSubject("Test Mail Subject");
+        message.setSubject("Verificar cuenta");
+        String link = "https://login.victoranchondo.com";
 
-        String msg = "This is my first email using JavaMailer";
-
+        String msg = getEmailBody(link);
         MimeBodyPart mimeBodyPart = new MimeBodyPart();
         mimeBodyPart.setContent(msg, "text/html; charset=utf-8");
 
@@ -43,6 +62,44 @@ public class EmailService {
         message.setContent(multipart);
 
         Transport.send(message);
+    }
+
+    private String getEmailBody(String link) throws IOException, TemplateException {
+        // Creates the freemarker configuration
+        Configuration cfg = getConfiguration("/templates/email");
+
+        // Set the properties
+        Map<String, Object> properties = getEmailPropertyMap(link);
+
+        // Get the template
+        Template template = cfg.getTemplate("emailValidation.ftl");
+
+        // Write the freemarker output to a StringWriter
+        StringWriter stringWriter = new StringWriter();
+        template.process(properties, stringWriter);
+
+        // Get the String from StringWritter
+        return stringWriter.toString();
+    }
+
+    private Configuration getConfiguration(String templatesFolder) {
+        Configuration cfg = new Configuration(Configuration.DEFAULT_INCOMPATIBLE_IMPROVEMENTS);
+        // Set templates folder
+        cfg.setClassForTemplateLoading(this.getClass(), templatesFolder);
+
+        // Recommended settings
+        cfg.setDefaultEncoding("UTF-8");
+        cfg.setLocale(Locale.US);
+        cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
+
+        return cfg;
+    }
+
+    private Map<String, Object> getEmailPropertyMap(String link) {
+        Map<String, Object> properties = new HashMap<>();
+        properties.put("link", link);
+
+        return properties;
     }
 
 }
---
+++ src/main/resources/templates/email/emailValidation.ftl
@@ -0,0 +1,7 @@
+<h1>Bienvenido a victoranchondo.com!</h1>
+<p>Gracias por crear tu cuenta, para terminar el registro haz click en el enlace o botón de abajo.</p>
+<p>
+    <a style="background:#ff6f3d;height:46px;margin:10px 0;padding:0 40px;line-height:46px;border-radius:4px;font-size:18px;color:#fff;display:inline-block"
+    href="${link}" title="Activar cuenta" target="_blank" data-saferedirecturl="https://www.google.com/url?q=${link}">Activar cuenta</a></p>
+<br />
+<p>Si usted no realizó el registro puede ignorar este mensaje.</p>
