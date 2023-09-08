61398104bb4a8658dd22dd641cc03d638c5e0fa1
vanchondo <eduardoanchondo@gmail.com>
2023-09-07 19:08:03 -0600

    Updating username regex

--- src/main/java/com/vanchondo/sso/utilities/RegexConstants.java
+++ src/main/java/com/vanchondo/sso/utilities/RegexConstants.java
@@ -1,6 +1,6 @@
 package com.vanchondo.sso.utilities;
 public abstract class RegexConstants {
   public static final String PASSWORD_REGEX = "^.{7,50}$";
-  public static final String USERNAME_REGEX = "^[A-Za-z][A-Za-z0-9_]{5,29}$";
+  public static final String USERNAME_REGEX = "^[A-Za-z][A-Za-z0-9_\\.]{5,29}$";
   public static final String EMAIL_REGEX = "^(.+)@(\\S+)$";
 }
