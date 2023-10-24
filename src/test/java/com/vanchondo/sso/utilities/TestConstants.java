39d0c396c182637704683310379b61d7010227a5
vanchondo <eduardoanchondo@gmail.com>
2023-10-24 08:36:50 -0600

    Updating secret key for unit tests

--- src/test/java/com/vanchondo/sso/utilities/TestConstants.java
+++ src/test/java/com/vanchondo/sso/utilities/TestConstants.java
@@ -5,7 +5,7 @@ public final class TestConstants {
 
   public static String TOKEN_ISSUER = "sso-svc";
   public static int TOKEN_EXPIRATION = 10;
-  public static String TOKEN_SECRET_KEY = "85b6396b76c702178d452532c7a63cdf598a932d5f9c4acd007799f7642d1c1e";
+  public static String TOKEN_SECRET_KEY = "WWxoc1ZHUllRbXhqYTFJeFkwZFdlVlV5Vm1wamJWWXdVekpXTlE9PQ==";
   public static String EMAIL = "victor@email.com";
   public static String USERNAME = "validUsername";
 
