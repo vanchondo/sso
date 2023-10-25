893839145a2ade36fd38b7b409bd72a56ad836af
vanchondo <eduardoanchondo@gmail.com>
2023-10-25 16:56:43 -0600

    Adding empty test class

---
+++ src/test/java/com/vanchondo/sso/services/AuthenticationServiceTest.java
@@ -0,0 +1,13 @@
+package com.vanchondo.sso.services;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith({SpringExtension.class})
+public class AuthenticationServiceTest {
+  @Mock
+  private UserService userService;
+
+
+}
