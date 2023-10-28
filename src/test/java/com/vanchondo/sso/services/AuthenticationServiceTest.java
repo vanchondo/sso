b48a3b863be40f26e9f083fc385b74cc1c45bcf7
Victor Anchondo <eduardoanchondo@gmail.com>
2023-10-28 17:44:08 -0600

    Adding getSigningKey unit test

--- src/test/java/com/vanchondo/sso/services/AuthenticationServiceTest.java
+++ src/test/java/com/vanchondo/sso/services/AuthenticationServiceTest.java
@@ -1,13 +1,31 @@
 package com.vanchondo.sso.services;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import com.vanchondo.sso.configs.properties.LoginConfiguration;
+import com.vanchondo.sso.utilities.TestConstants;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith({SpringExtension.class})
 public class AuthenticationServiceTest {
   @Mock
   private UserService userService;
+  @Mock
+  private PasswordEncoder passwordEncoder;
+  @Mock
+  private LoginConfiguration loginConfiguration;
+  @InjectMocks
+  private AuthenticationService authenticationService;
+
+  @Test
+  public void testGetSigningKey() {
+    assertNotNull(AuthenticationService.getSigningKey(TestConstants.TOKEN_SECRET_KEY));
+  }
 
 
 }
